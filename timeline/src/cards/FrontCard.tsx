acf85a42d3064ce270d52d3de2465f786543317f
Clemence Boucher <clemence.boucher@unistra.fr>
2025-04-09 15:26:22 +0200

    Cate front faite

--- timeline/src/cards/FrontCard.tsx
+++ timeline/src/cards/FrontCard.tsx
@@ -3,15 +3,20 @@ import { Carte } from "../types";
 
 export default function Card({ card }: { card: Carte }) {
   return (
-    <div className="bg-gray-800 rounded-2xl shadow-lg p-6 text-white w-full max-w-md">
-      <h2 className="text-xl font-bold text-center mb-4">{card.titre}</h2>
-
-      <div className="flex flex-col gap-2 text-sm">
-        <p className="font-semibold"> Thématique : {card.thematique}</p>
-        <p className="font-semibold"> Type : {card.type}</p>
-        <p className="font-semibold"> Titre : {card.titre}</p>
-        <p className="font-semibold"> Date : {card.date}</p>
+    <div className="bg-[#E5ECF4] rounded-2xl shadow-lg  pb-8 text-black flex flex-col items-center justify-center gap-2 text-sm w-64">
+      <div className="bg-[#2D2DA0] font-semibold rounded-3xl py-1 text-white w-full">
+        {card.thematique}
       </div>
+
+      <div className="bg-[#E63946] font-semibold rounded-2xl py-1 mb-4 text-white w-16">
+        {card.date}
+      </div>
+
+      <p className="text-xl font-bold text-center mb-4= text-[#E63946] break-words">
+        {card.titre}
+      </p>
+
+      <p className="italic text-[#E63946]">{card.type}</p>
     </div>
   );
 }
