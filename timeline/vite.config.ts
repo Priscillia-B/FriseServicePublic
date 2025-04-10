6e89fd2bf9a60dcf7d2c85e9fcfe8169259f2c99
bruckerp <priscillia.brucker@etu.unistra.fr>
2025-04-10 21:24:53 +0200

    correction github vite.config

--- timeline/vite.config.ts
+++ timeline/vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'FriseServicePublic',
+  base: '/FriseServicePublic',
   plugins: [react(), tailwindcss()],
 });
