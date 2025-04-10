555e3cc35c7cbf9e136c3597dd76af8f817d1854
nathan.bowman <nathan.bowman@etu.unistra.fr>
2025-04-10 08:59:16 +0200

    avancement

--- timeline/src/cards/FrontCard.tsx
+++ timeline/src/cards/FrontCard.tsx
@@ -1,18 +1,19 @@
-import React from "react";
-import { Carte } from "../types";
+// src/cards/FrontCard.tsx
+import React from 'react';
+import { Carte } from '../types';
 
-export default function Card({ card }: { card: Carte }) {
+export default function FrontCard({ card }: { card: Carte }) {
   return (
-    <div className="bg-[#E5ECF4] rounded-2xl shadow-lg  pb-8 text-black flex flex-col items-center justify-center gap-2 text-sm w-64">
-      <div className="bg-[#2D2DA0] font-semibold rounded-3xl py-1 text-white w-full">
+    <div className="bg-[#E5ECF4] rounded-2xl shadow-lg pb-8 text-black flex flex-col items-center justify-center gap-2 text-sm w-64">
+      <div className="bg-[#2D2DA0] font-semibold rounded-3xl py-1 text-white w-full text-center">
         {card.thematique}
       </div>
 
-      <div className="bg-[#E63946] font-semibold rounded-2xl py-1 mb-4 text-white w-16">
+      <div className="bg-[#E63946] font-semibold rounded-2xl py-1 mb-4 text-white w-16 text-center">
         {card.date}
       </div>
 
-      <p className="text-xl font-bold text-center mb-4= text-[#E63946] break-words">
+      <p className="text-xl font-bold text-center mb-4 text-[#E63946] break-words">
         {card.titre}
       </p>
 
