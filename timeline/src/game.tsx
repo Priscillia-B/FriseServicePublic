466a9c5351146ba4e570c0094a82bee34850ec9d
Samuel Chanal <samuel.chanal@etu.unistra.fr>
2025-04-10 10:54:46 +0200

    Mise en place de fonctions pour gérer le deck

--- timeline/src/game.tsx
+++ timeline/src/game.tsx
@@ -1,25 +1,63 @@
 // src/pages/Game.tsx
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import fetchData from './api';
 import { Carte } from './types';
 import { TimelineBoard } from './components/TimelineBoard';
 
 export default function Game() {
-  const [cards, setCards] = useState<Carte[]>([]);
+  const [deck, setDeck] = useState<Carte[]>([]);
+  const [placedCards, setPlacedCards] = useState<Carte[]>([]);
+  const [currentCardIndex, setCurrentCardIndex] = useState<number>(0);
 
-  // Exemple de fetch asynchrone (si tu utilises fetchData)
+  // Temporaire (peut-être)
+  const hasPlacedCard = useRef(false);
+
   useEffect(() => {
     fetchData().then((data) => {
-      setCards(data);
+      console.log("Data fetched:", data);
+      setDeck(data);
+      setCurrentCardIndex(Math.floor(Math.random() * data.length));
     });
   }, []);
+
+  useEffect(() => {
+    if (deck.length > 0 && !hasPlacedCard.current) {
+      placeCard(currentCardIndex, 0);
+      hasPlacedCard.current = true;
+      }
+  }, [deck]);
+
+  //Fonction qui prend deux index de carte, et transfert la carte du premier index du deck vers placedCards à l'emplacement du second index
+  const placeCard = (indexFrom: number, indexTo: number) => {
+    const cardToPlace = deck[indexFrom];
+    const newDeck = [...deck];
+    newDeck.splice(indexFrom, 1);
+    setDeck(newDeck);
+
+    const newPlacedCards = [...placedCards];
+    if (indexTo >= newPlacedCards.length) {
+      newPlacedCards.push(cardToPlace);
+    } else {
+      newPlacedCards.splice(indexTo, 0, cardToPlace);
+    }
+    setPlacedCards(newPlacedCards);
+  }
+
+  //Fonction pour récupérer la carte en cours
+  const getCurrentCard = () => {
+    if (deck.length > 0) {
+      return deck[currentCardIndex];
+    }
+    return null;
+  }
+    
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-white">
       <h1 className="text-3xl font-bold mb-8">Jeu TimeLine</h1>
       
       {/* TimelineBoard */}
-      <TimelineBoard cards={cards} />
+      <TimelineBoard cards={placedCards} />
     </div>
   );
 }
